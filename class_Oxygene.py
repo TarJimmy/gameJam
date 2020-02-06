8bb29193a7c4a286db971b260405a475f38ce9c4
TarJimmy <tardyjim26@gmail.com>
2020-02-06 23:48:44 +0100

    save

--- class_Oxygene.py
+++ class_Oxygene.py
@@ -6,7 +6,6 @@ class Oxygene(Object):
     def __init__(self,x,y,cheminImage = "images/objects/oxygene.png", type = "oxygene"):
         super().__init__(x,y,cheminImage, type)
         self.bonus = random.randint(100, 300)
-        self.redimensionne(80,65)
 
     def test(self, player):
         return self.rect.colliderect(pygame.Rect(player.x,player.y,player.width,player.height))
